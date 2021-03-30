0248b0badbac58bbf21ee41e37e8776e599b3b6b
larryshaw0079 <larryshaw0079@live.com>
2021-03-30 16:11:25 +0800

    add svm template

---
+++ baseline/svm.py
@@ -0,0 +1,112 @@
+"""
+@Time    : 2021/3/30 16:06
+@Author  : Xiao Qinfeng
+@Email   : qfxiao@bjtu.edu.cn
+@File    : svm.py
+@Software: PyCharm
+@Desc    : 
+"""
+import argparse
+import os
+import pickle
+import random
+import shutil
+import warnings
+
+import numpy as np
+import torch
+
+from sklearn.model_selection import KFold
+from tqdm.std import tqdm
+
+from mme.dataset import SEED_NUM_SUBJECT, DEAP_NUM_SUBJECT, AMIGOS_NUM_SUBJECT
+
+
+def setup_seed(seed):
+    warnings.warn(f'You have chosen to seed ({seed}) training. This will turn on the CUDNN deterministic setting, '
+                  f'which can slow down your training considerably! You may see unexpected behavior when restarting '
+                  f'from checkpoints.')
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+def parse_args(verbose=True):
+    parser = argparse.ArgumentParser()
+
+    # Dataset
+    parser.add_argument('--data-path', type=str, default='/data/DataHub/EmotionRecognition/SEED/Preprocessed_EEG')
+    parser.add_argument('--data-name', type=str, default='SEED', choices=['SEED', 'DEAP', 'AMIGOS'])
+    parser.add_argument('--save-path', type=str, default='./cache/tmp')
+    parser.add_argument('--classes', type=int, default=3)
+
+    # Training
+    parser.add_argument('--kfold', type=int, default=10)
+    parser.add_argument('--fold', type=int, default=0)
+    parser.add_argument('--resume', action='store_true')
+
+    parser.add_argument('--seed', type=int, default=2020)
+
+    args_parsed = parser.parse_args()
+
+    if verbose:
+        message = ''
+        message += '-------------------------------- Args ------------------------------\n'
+        for k, v in sorted(vars(args_parsed).items()):
+            comment = ''
+            default = parser.get_default(k)
+            if v != default:
+                comment = '\t[default: %s]' % str(default)
+            message += '{:>35}: {:<30}{}\n'.format(str(k), str(v), comment)
+        message += '-------------------------------- End ----------------------------------'
+        print(message)
+
+    return args_parsed
+
+
+def run(run_id, train_patients, test_patients, args):
+    print('Train patient ids:', train_patients)
+    print('Test patient ids:', test_patients)
+
+    #############################################################
+    #                       Your code here                      #
+    #############################################################
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    if args.seed is not None:
+        setup_seed(args.seed)
+
+    if not os.path.exists(args.save_path):
+        warnings.warn(f'The path {args.save_path} dost not existed, created...')
+        os.makedirs(args.save_path)
+    elif not args.resume:
+        warnings.warn(f'The path {args.save_path} already exists, deleted...')
+        shutil.rmtree(args.save_path)
+        os.makedirs(args.save_path)
+
+    if args.data_name == 'SEED':
+        num_patients = SEED_NUM_SUBJECT
+    elif args.data_name == 'DEAP':
+        num_patients = DEAP_NUM_SUBJECT
+    elif args.data_name == 'AMIGOS':
+        num_patients = AMIGOS_NUM_SUBJECT
+    else:
+        raise ValueError
+
+    patients = np.arange(num_patients)
+
+    assert args.kfold <= len(patients)
+    assert args.fold < args.kfold
+    kf = KFold(n_splits=args.kfold)
+    for i, (train_index, test_index) in enumerate(kf.split(patients)):
+        if i == args.fold:
+            print(f'[INFO] Running cross validation for {i + 1}/{args.kfold} fold...')
+            train_patients, test_patients = patients[train_index].tolist(), patients[test_index].tolist()
+            run(i, train_patients, test_patients, args)
+            break
